46a1496d60bbc510601cb5da0392ae34f7f7b073
robwatsongtr <rwatso@gmail.com>
2024-06-26 18:04:12 -0700

    added create and update functions to models

--- models/clientModel.go
+++ models/clientModel.go
@@ -3,11 +3,11 @@ package models
 import "database/sql"
 
 type Client struct {
-	ID          int
-	Name        string
-	Child_name  string
-	Email       string
-	Billing_amt float32
+	Client_id   int     `json:"client_id"`
+	Name        string  `json:"name"`
+	Child_name  string  `json:"child_name"`
+	Email       string  `json:"email"`
+	Billing_amt float32 `json:"billing_amt"`
 }
 
 func GetClients(db *sql.DB) ([]Client, error) {
@@ -25,7 +25,7 @@ func GetClients(db *sql.DB) ([]Client, error) {
 	// As long as there's a row, read the row into a client object and save into client slice
 	for rows.Next() {
 		var client Client
-		if err := rows.Scan(&client.ID, &client.Name, &client.Child_name, &client.Email,
+		if err := rows.Scan(&client.Client_id, &client.Name, &client.Child_name, &client.Email,
 			&client.Billing_amt); err != nil {
 			return nil, err
 		}
@@ -33,4 +33,21 @@ func GetClients(db *sql.DB) ([]Client, error) {
 	}
 
 	return clients, nil
+}
+
+func CreateClient(db *sql.DB, client *Client) error {
+	query := `INSERT INTO clients (name, child_name, email, billing_amt) VALUES ($1, $2, $3, $4) 
+		RETURNING client_id`
+	err := db.QueryRow(query, client.Name, client.Child_name,
+		client.Email, client.Billing_amt).Scan(&client.Client_id)
+
+	return err
+}
+
+func UpdateClient(db *sql.DB, client *Client) error {
+	query := "UPDATE clients SET name=$1, child_name=$2, email=$3, billing_amt=$4 WHERE client_id=$5"
+	_, err := db.Exec(query, client.Name, client.Child_name, client.Email,
+		client.Billing_amt, client.Client_id)
+
+	return err
 }
